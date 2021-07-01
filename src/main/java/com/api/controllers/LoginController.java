7ea284fa5ec2308a2551ad986f2a499c80fba562
adnan khan <aka034437@gmail.com>
2021-07-01 20:52:13 +0530

    modified files

--- src/main/java/com/api/controllers/LoginController.java
+++ src/main/java/com/api/controllers/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.config.JwtTokenUtil;
@@ -25,6 +26,7 @@ import com.api.services.SmsService;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/login")
 public class LoginController {
 	
 	@Autowired
--- src/main/java/com/api/controllers/RegistrationController.java
+++ src/main/java/com/api/controllers/RegistrationController.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.security.core.userdetails.UserDetailsService;
 //import org.springframework.messaging.simp.SimpMessagingTemplate;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.config.JwtTokenUtil;
@@ -25,6 +27,8 @@ import com.api.services.SmsService;
 import com.api.services.StudentService;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/registration")
 public class RegistrationController {
 	
 	@Autowired
